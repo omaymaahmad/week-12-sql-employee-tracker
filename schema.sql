139459264ed542392899fddee2ef68abe3d964ca
omayma ahmad <omayma.ahmad25@gmail.com>
2021-01-27 20:59:56 +0000

    added to tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,18 @@ PRIMARY KEY (id)
 );
 
 
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary INT DECIMAL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL 
+)
