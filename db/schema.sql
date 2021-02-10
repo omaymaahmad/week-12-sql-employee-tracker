fca8e44e506d2ded279c29c34d9b882b42d18dfd
omayma ahmad <omayma.ahmad25@gmail.com>
2021-02-10 19:08:55 +0000

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,7 @@ PRIMARY KEY (id)
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary INT DECIMAL (10,2) NOT NULL,
+    salary INT NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
@@ -26,6 +26,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    manager_id INT NULL,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE 
+    manager_id INT,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+    PRIMARY KEY (id) 
 );
